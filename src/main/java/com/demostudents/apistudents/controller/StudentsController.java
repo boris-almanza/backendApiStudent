91f325428c5cd35bd05ce2e95cf72bbce908cb58
boris <boris14101@gmail.com>
2022-10-14 13:43:11 -0300

    add annotation on add method controller

--- src/main/java/com/demostudents/apistudents/controller/StudentsController.java
+++ src/main/java/com/demostudents/apistudents/controller/StudentsController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,7 +22,7 @@ public class StudentsController {
     private StudentService studentService;
 
     @PostMapping("/add")
-    public String add( Student student) {
+    public String add(@RequestBody Student student) {
         studentService.saveStudent(student);
         return "New Student is add";
     }
